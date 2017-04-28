6896a0e6efbf032e09fec15ef48d284966d56e9f
THUSHAN <thushankavishka@gmail.com>
2017-04-28 18:18:18 +0530

    Commenting on main.cpp file

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //main method
 int main()
 {
-    //print hello world and return 0
+    //print hello world and return 0...
     cout << "Hello world!" << endl;
     return 0;
 }
